2c7f60a29a14e2425a21cfcd92e1aa02087f836f
Sebastian Menski <sebastian.menski@googlemail.com>
2014-08-07 09:46:04 +0200

    add SLF4J with simple logger to test case

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,17 +12,20 @@
  */
 package org.camunda.bpm.unittest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-
-import static org.junit.Assert.*;
-
 import org.junit.Rule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Daniel Meyer
@@ -33,25 +36,39 @@ public class SimpleTestCase {
   @Rule
   public ProcessEngineRule rule = new ProcessEngineRule();
 
+  public static final Logger LOG = LoggerFactory.getLogger(SimpleTestCase.class);
+
   @Test
   @Deployment(resources = {"testProcess.bpmn"})
   public void shouldExecuteProcess() {
 
+    LOG.info("Starting test case");
+
     RuntimeService runtimeService = rule.getRuntimeService();
     TaskService taskService = rule.getTaskService();
+
+    LOG.debug("Got runtime- and taskService");
 
     ProcessInstance pi = runtimeService.startProcessInstanceByKey("testProcess");
     assertFalse("Process instance should not be ended", pi.isEnded());
     assertEquals(1, runtimeService.createProcessInstanceQuery().count());
 
+    LOG.info("Process started");
+
     Task task = taskService.createTaskQuery().singleResult();
     assertNotNull("Task should exist", task);
+
+    LOG.debug("Found task " + task);
 
     // complete the task
     taskService.complete(task.getId());
 
+    LOG.info("Task completed");
+
     // now the process instance should be ended
     assertEquals(0, runtimeService.createProcessInstanceQuery().count());
+
+    LOG.info("Process ended");
 
   }
 
