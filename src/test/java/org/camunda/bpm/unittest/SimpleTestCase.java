8e70a65088a830fd87a6bd3d8f177f9c20cd96a5
Sebastian Menski <sebastian.menski@googlemail.com>
2015-02-12 16:41:44 +0100

    add example for call activity variable propagation

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,12 +12,13 @@
  */
 package org.camunda.bpm.unittest;
 
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.task;
+
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -31,21 +32,19 @@ public class SimpleTestCase {
   public ProcessEngineRule rule = new ProcessEngineRule();
 
   @Test
-  @Deployment(resources = {"testProcess.bpmn"})
+  @Deployment(resources = {"process1.bpmn", "process2.bpmn", "process3.bpmn"})
   public void shouldExecuteProcess() {
     // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
+    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("Process_1");
+
     // And there should exist just a single task within that process instance
     assertThat(task(processInstance)).isNotNull();
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
+    // And the process variables exist
+    assertThat(processInstance).variables()
+      .containsEntry("hello", "world")
+      .containsEntry("a", "?")
+      .containsEntry("b", "");
   }
 
 }
